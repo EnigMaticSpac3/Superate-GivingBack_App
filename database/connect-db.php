4777072d35e991fd6105dd0dcaa16a200e161bda
EnigMaticSpac3 <58480988+Jorge25G@users.noreply.github.com>
2021-11-30 15:19:50 -0500

    html to php added database stuff

---
+++ database/connect-db.php
@@ -0,0 +1,11 @@
+<!-- Modify this part in order to connect to the database -->
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "practice";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $db);
+?>
+

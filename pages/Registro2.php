67df32b606fea67cdb8d06ca2d3e93560b5b9fc7
EnigMaticSpac3 <58480988+Jorge25G@users.noreply.github.com>
2021-12-08 20:42:51 -0500

    addPost Page & other addings
    Co-authored-by: Daphne Gonzalez <daphne.gonzalez2021@jupa.superate.org.pa>

--- pages/Registro2.php
+++ pages/Registro2.php
@@ -20,13 +20,14 @@
 <head>
     <?php include "../includes/meta-inc.php" ?>
     <link rel="stylesheet" href="../resources/style/mainInicio.css">
-    <link rel="stylesheet" href="../resources/style/r2.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <link rel="stylesheet" href="../resources/style/Hidden.css">
     <title>Registro</title>
 </head>
 <body>
     <p class="hidden message">_No compatible con este dispositivo.</p>
 
-    <div class="content">
+    <!-- <div class="content">
         <div class="center title">
             <h1>Registro</h1>
         </div>
@@ -49,12 +50,34 @@
                         <input placeholder="Confirmar su contraseña" class="input" type="password" name="user_pwd2" id="pass2" required=""><span class="showp">Mostrar</span><span class="barra"></span>
                     </div>
                 </div>
-                <input class="button textcenter" name="next2" type="submit" value="Siguiente">
-
+                <input class="button textcenter" name="next2" type="submit" value="Siguiente"> -->
+<!-- 
         </form>
     </div>
-
-
+ -->
+ <div class="content">
+          <div class="center title">
+              <h1>Registro</h1>
+          </div>
+          <form class="formb" action="./Registro3.php" method="post">
+              <div class="label">
+                  <label for="">Email</label>
+                  <input placeholder="Ingrese su correo electronico" class="input" type="email" name="email" id="name" required><span class="barra"></span>
+              </div>
+              <div class="label">
+               <label for="password">Contraseña</label>
+                 <input placeholder="Ingrese su contraseña" class="input" type="password" name="user_pwd" autocomplete="current-password" required="" id="id_password">
+                 <i class="far fa-eye inline togglePassword1" id="togglePassword"  style="cursor: pointer;"></i>
+             </div>
+             <div class="label">
+               <label for="password">Contraseña</label>
+                 <input placeholder="Ingrese su contraseña" class="input" type="password" name="password" autocomplete="current-password" required="" id="id_password">
+                 <i class="far fa-eye inline togglePassword1" id="togglePassword"  style="cursor: pointer;"></i>
+             </div>
+             <input class="button textcenter" name="next2" type="submit" value="Siguiente">
+          </form>
+      </div>
+    <script src="../resources/scripts/script2.js"></script>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="../resources/scripts/script.js"></script>
